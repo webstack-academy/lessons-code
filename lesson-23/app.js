f2c13cf5822b3e711194cdbf7bd5726abe282558
om <om>
2022-12-03 14:50:15 +0100

    fix: added the correct code

--- lesson-23/app.js
+++ lesson-23/app.js
@@ -1,49 +1,17 @@
-const express = require('express')
+import express from 'express'
+
 const app = express()
 const port = 3000
-const productRouter = require('./product.route')
 
+import { router as productRoutes} from './products/product.route.js'
 
 app.use(express.json())
-app.use('/product', productRouter)
-
-const products = []
-// {
-//   details: 'string',
-//     price: 12
-// }
-// requirements
-// deiltals.length > 10
-// price > 0
-// res.locals
-const productsValidator = (req, res, next) => {
-  const details = req.body.details
-  const price = req.body.price
-
-  if(details.length > 10 && price > 0){
-    res.locals.details = details
-    res.locals.price = price
-
-    return next()
-  }
-  else{
-    res.status(400)
-    res.send('I parametri inseriti non sono corretti')
-  }
-}
-
-app.post('/', productsValidator, (req, res) => {
-  products.push({
-    details: res.locals.details,
-    price: res.locals.price
-  })
-  res.send({ message: 'Parametri aggiunti' })
-})
-
-app.get('/', (req, res) => {
-    res.send({ products })
-})
+app.use('/product', productRoutes)
+app.get('/', (req, res) =>{
+    res.send('hello world')
+} )
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
